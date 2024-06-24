be5faf49c0a7987cb9af515fe73a94da8b417e93
xfz1987 <281345774@qq.com>
2024-06-24 09:48:09 +0800

    fix middleware

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,34 @@
-import { authMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
 import createMiddleware from 'next-intl/middleware';
-import { LOCALES } from './i18n';
+import { LOCALES, DEFAULT_LOCALE } from './i18n';
+
+// 国际化中间件
 const intlMiddleware = createMiddleware({
 	locales: LOCALES,
-	defaultLocale: 'en',
+	defaultLocale: DEFAULT_LOCALE,
 });
 
-export default authMiddleware({
-	beforeAuth(request) {
-		if (!request.url.includes('/api')) {
-			return intlMiddleware(request);
-		}
-	},
-	publicRoutes: ['/:locale/sign-in', '/:locale/sign-up', '/api/:path*'],
+const isPublicRoute = createRouteMatcher([
+	'/:locale/sign-in(.*)',
+	'/sign-in(.*)',
+	'/:locale/sign-up(.*)',
+	'/sign-up(.*)',
+	'/api/snippet(.*)',
+]);
+
+export default clerkMiddleware((auth, request) => {
+	if (!isPublicRoute(request)) {
+		auth().protect();
+	}
+
+	if (!request.url.includes('/api')) {
+		return intlMiddleware(request);
+	}
 });
 
+// 中间件匹配原则，不在这个范围内则不会执行 middleware 逻辑
 export const config = {
+	// matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 	matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
